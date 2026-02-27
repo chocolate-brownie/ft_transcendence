30ba7a75d7f779756cffdef3b60e9c4a1ce773b0
darkwhite.off@gmail.com <ziggy_@DESKTOP-KO57H8D.localdomain>
2026-02-27 15:46:52 +0100

    feat: board component issue #60 - Added a button on GameLobby that redirects to Game/demo to preview the board

--- frontend/src/pages/Game.tsx
+++ frontend/src/pages/Game.tsx
@@ -1,7 +1,25 @@
+import { useState } from "react";
+import type { Board } from "../types/game";
+import GameBoard from "../components/GameBoard";
+
 export default function Game() {
+  const [board, setBoard] = useState<Board>(Array(9).fill(null));
+  const [isXTurn, setIsXTurn] = useState<boolean>(true);
+
+  function handleCellClick(index: number) {
+    if (board[index] !== null) return;
+
+    const nextBoard = [...board];
+    nextBoard[index] = isXTurn ? "X" : "O";
+
+    setBoard(nextBoard);
+    setIsXTurn(!isXTurn);
+}
+
   return (
-    <div>
+    <div className="flex flex-col items-center gap-4">
       <h1 className="text-2xl font-bold text-pong-text">Game</h1>
+      <GameBoard board={board} onCellClick={handleCellClick} />
     </div>
   );
 }
