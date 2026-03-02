2f057a2da70bb656c863aa219bb4331608cd4480
Pollos <pollos@pablo.localdomain>
2026-03-02 22:15:03 +0100

    feat: local game mode #97 - corrected winning lines possibilities

--- frontend/src/pages/Game.tsx
+++ frontend/src/pages/Game.tsx
@@ -10,14 +10,9 @@ import Button from "../components/Button";
 
 // Every possible winning outcome
 const WIN_LINES: number[][] = [
-  [0, 1, 2], // horizontal
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6], // vertical
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8], // diagonal
-  [2, 4, 6],
+  [0, 1, 2], [3, 4, 5], [6, 7, 8], // horizontal
+  [0, 3, 6], [1, 4, 7], [2, 5, 8], // vertical
+  [0, 4, 8], [2, 4, 6],            // diagonal
 ];
 
 // Function to check a line
