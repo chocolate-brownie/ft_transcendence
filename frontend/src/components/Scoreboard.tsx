496bc1a955a525febdf44d8de7c07e0d9bb33fa6
Saprebleu <NOREP.net>
2026-03-04 17:23:07 +0100

    interactive scoreboard

---
+++ frontend/src/components/Scoreboard.tsx
@@ -0,0 +1,99 @@
+import { useState } from "react";
+import { useAuth } from "../context/AuthContext";
+import { User } from "../types/index"
+
+/* Displays players' information under the Game board  */
+type ScoreboardProps = {
+  isMyTurn: boolean;
+};
+
+export const Scoreboard = ({isMyTurn}: ScoreboardProps): JSX.Element | null => {
+
+  const { user } = useAuth();
+  const myAvatar = user.avatarUrl ?? "/default-avatar.png";
+  const myUsername = user?.username;
+
+  // To set according to the opponent's info:
+  let otherUsername = "nickname";
+  let otherAvatar = "/default-avatar.png";
+
+  // isMyTurn = false;
+  if (!user) return (<span></span>);
+  
+  return (
+      <div className="rounded-lg bg-pong-surface py-2 shadow-sm">
+        <div className="flex items-center gap-8 text-pong-text/80">
+
+          {/* Player 1 */}
+          <div className="flex flex-col items-center w-28 gap-2">
+            <span
+              className={`
+              font-semibold text-lg tracking-wide pb-2
+              ${isMyTurn ? 'text-pong-accent' : 'text-pong-text/50'}
+              `}>
+              {isMyTurn ? "Plays" : "Waits..."}
+            </span>
+            {/* Image shadow changes if it is my turn or not */}
+            <img
+            className={
+              isMyTurn
+              ? "w-20 h-20 rounded-full object-cover shadow-[0_0_20px_5px_rgba(246,185,111,1)]"
+              : "w-20 h-20 rounded-full object-cover opacity-70"
+            }
+            src={myAvatar}
+            alt="your_avatar"
+            />
+    
+            <div className="flex flex-row text-sm w-40 justify-center py-15"> 
+              <span>
+                {myUsername}
+                <span className="text-pong-accent">
+                  (X)
+                </span>
+              </span>
+            </div>
+          </div>
+
+
+          {/* Ties (optionnel, comme sur ton screenshot) */}
+          <div className="flex flex-col items-center px-7">
+            <span className="text-xl font-semibold uppercase tracking-wide text-pong-text/50">
+              VS
+            </span>
+          </div>
+
+          {/* Player 2 */}
+          <div className="flex flex-col items-center w-28 gap-2">
+            <span
+              className={`
+              font-semibold text-lg tracking-wide pb-2
+              ${isMyTurn ? 'text-pong-text/50' : 'text-pong-secondary' }
+              `}>
+              {isMyTurn ? "Waits..." : "Plays" }
+            </span>
+            {/* Image shadow changes if it is my turn or not */}
+            <img
+            className={
+              isMyTurn
+              ? "w-20 h-20 rounded-full object-cover opacity-70"
+              : "w-20 h-20 rounded-full object-cover shadow-[0_0_20px_5px_rgba(117,153,51,1)]"
+            }
+            // src={otherAvatar}
+            src={myAvatar}
+            alt="opponents_avatar"
+            />
+        <div className="flex flex-row text-sm w-40 justify-center py-15"> 
+          <span>
+            {otherUsername}
+            <span className="text-pong-secondary">
+              (O)
+            </span>
+          </span>
+        </div>
+        </div>
+
+      </div>
+    </div>
+  );
+
+};
--- frontend/src/pages/Game.tsx
+++ frontend/src/pages/Game.tsx
@@ -4,6 +4,7 @@ import GameBoard from "../components/Game/GameBoard";
 import TurnIndicator from "../components/Game/TurnIndicator";
 import Button from "../components/Button";
 import { findWinningLine } from "../utils/gameUtils";
+import { Scoreboard } from "../components/Scoreboard";
 
 //TEMP FOR DEV (maybe make it a backend issue to send winning line info? or just calculate it on the frontend?)
 // TODO: when backend game API is wired to this page,
@@ -43,12 +44,16 @@ export default function Game() {
         isYourTurn={isYourTurn}
         className="-mb-6"
       />
+      
+      
       {/* Board */}
       <GameBoard
       board={board}
       onCellClick={handleCellClick}
       winningLine={winningLine}
       />
+      
+      
       {/* New / Quit button (placeholders Phase 4) */}
       <div className="mt-7 flex gap-5">
         <Button variant="primary">
@@ -58,38 +63,11 @@ export default function Game() {
           Quit Game
         </Button>
       </div>
+      
+      
       {/* Scoreboard / Player vs Player */}
       <div className="rounded-lg bg-pong-surface px-12 py-2 shadow-sm">
-        <div className="flex items-center gap-8 text-pong-text/80">
-          {/* Player 1 */}
-          <div className="flex flex-col items-center px-3">
-            <span className="font-semibold uppercase tracking-wide text-pong-text/50">
-              Player 1
-            </span>
-            <span className="text-sm">
-              You <span className="text-pong-accent">(X)</span>
-            </span>
-            <span className="text-3xl font-sans font-bold text-pong-accent">0</span>
-          </div>
-
-          {/* Ties (optionnel, comme sur ton screenshot) */}
-          <div className="flex flex-col items-center px-7">
-            <span className="text-xl font-semibold uppercase tracking-wide text-pong-text/50">
-              VS
-            </span>
-          </div>
-
-          {/* Player 2 */}
-          <div className="flex flex-col items-center">
-            <span className="font-semibold uppercase tracking-wide text-pong-text/50">
-              Player 2
-            </span>
-            <span className="text-sm">
-              Opponent <span className="text-pong-secondary">(O)</span>
-            </span>
-            <span className="text-3xl font-sans font-bold text-pong-secondary">0</span>
-          </div>
-        </div>
+        <Scoreboard isMyTurn={isYourTurn}></Scoreboard>
       </div>
     </div>
   );
